5f700db428cf4c1f7cc0989fe55fefc3a26ece7f
5usama <dhuddiusama5@gmail.com>
2025-07-27 06:11:12 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
+
+
+
+settings.SESSION_ID = "UsamaMD~HsIlwRZJ#UQcWrIqP4rs68RUNkjvmVBE1Kkl4Kq9blZ92K26Sk_E"
 //contact details
 global.ownernomer = "923239601585"
 global.ownername = "USAMA"
